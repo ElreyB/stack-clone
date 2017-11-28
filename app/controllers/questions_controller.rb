2561424d8acb22c42fc49eb82489ca173f014da8
Elrey Belmonti and Mitchell Long <mitchell@>
2017-11-28 13:33:37 -0800

    set questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,51 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:notice] = "Your question has ben saved!"
+      redirect_to questions_path
+    else
+      flash[:alert] = "Please fix these errors:"
+      render :new
+    end
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      flash[:notice] = "Your question has been update!"
+      redirect_to question_path(@question)
+    else
+      flash[:alert] = "Please fix these errors:"
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @quesiton.destroy
+    redirect_to questions_path
+  end
+
+
+private
+
+  def question_params
+    params.require(:question).permit(:title, :body, :net_vote)
+  end
 
 end
