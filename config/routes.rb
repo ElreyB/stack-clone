208b5b6cf0683024a746b7da346ae40cbc82aa5d
Elrey Belmonti and Mitchell Long <mitchell@>
2017-11-28 13:08:10 -0800

    set up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root :to => 'questions#index'
+
+  resources :questions do
+    resources :answers
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
